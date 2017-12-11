cfe8a3ae1745d6f916cd93cb185a323ab10d8600
rcky <ruefe@rcky.ch>
2017-12-11 16:30:01 +0100

    feat(table-alias-provider): adds table alias provider

---
+++ src/TableAliasProvider.js
@@ -0,0 +1,41 @@
+module.exports = class TableAliasProvider {
+    constructor(aliases = {}, separator = '_') {
+        this.aliases = aliases;
+        this.separator = separator;
+    }
+
+    createAlias(model, relation, { through } = {}) {
+        const key = this.createKey(model, relation, through);
+        const count = this.updateAndGetAliasCount(key);
+        return this.appendCount(key, count);
+    }
+
+    joinSegments(...segments) {
+        // This seems to be necessary due to the auto generation
+        // of the models which does not seem to preserve cases
+        return segments
+            .filter(Boolean)
+            .map(entry => entry.toString().toLowerCase())
+            .join(this.separator);
+    }
+
+    appendCount(key, count) {
+        return this.joinSegments(key, count);
+    }
+
+    updateAndGetAliasCount(key) {
+        if (!this.hasAlias(key)) {
+            this.aliases[key] = -1;
+        }
+        this.aliases[key] = this.aliases[key] + 1;
+        return this.aliases[key];
+    }
+
+    hasAlias(key) {
+        return Object.prototype.hasOwnProperty.call(this.aliases, key);
+    }
+
+    createKey(model, relation, through = null) {
+        return this.joinSegments(model, through, relation);
+    }
+};
